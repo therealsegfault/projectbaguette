627939fb35f8c6942d39f35afe1c91dff62b0b4d
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-04 11:22:36 -0800

    Update svelte.config.js

--- svelte/svelte-ui/svelte.config.js
+++ svelte/svelte-ui/svelte.config.js
@@ -1,17 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/projectbaguette' : ''
+			base: process.env.NODE_ENV === 'production'
+				? '/projectbaguette'
+				: ''
 		},
-		alias: {
-			$assets: 'static/assets'
+		prerender: {
+			handleHttpError: 'ignore' // avoids 404 issues on GH pages
 		}
 	}
 };
