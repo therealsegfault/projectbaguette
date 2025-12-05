4322dff70591e1b21f81bbdb14f18bafa67be156
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-05 07:38:13 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// js/main.js
+// Just wiring up the engine side effects
+import "./game/core.js";
+import "./game/audio.js";
+import "./game/notes.js";
+import "./game/input.js";
+import "./game/render.js";
+import "./game/loop.js";
+import "./game/start.js";
